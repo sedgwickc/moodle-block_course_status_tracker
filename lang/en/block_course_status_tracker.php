70dc2e100253853c1c06ae67032c0deb09b613a0
3i Logic LMS <lms@3ilogic.com>
2014-08-22 16:50:00 +0500

    Update block_course_status_tracker.php

--- lang/en/block_course_status_tracker.php
+++ lang/en/block_course_status_tracker.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -13,33 +14,31 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /* Course Status Tracker Block
  * The plugin shows the number and list of enrolled courses and completed courses.
  * It also shows the number of courses which are in progress and whose completion criteria is undefined but the manger.
  * @package blocks
  * @author: Azmat Ullah, Talha Noor
  */
-$string['pluginname']='Course Status Tracker';
-$string['course_status']='Course Status Tracker';
-$string['course_status_tracker']='Course Status Tracker';
-$string['s_no']='S. No.';
-$string['course_name']='Course Name';
-$string['course_comp_date']='Course Completion Date';
-$string['grade']='Grade';
-$string['course_status_tracker:view']='Course Status View';
-$string['course_status_tracker:addinstance']='Add Instance';
-$string['enrolled_courses']='Enrolled Courses';
-$string['completed_courses']='Completed Courses';
-$string['inprogress_courses']='Course In Progress';
-$string['undefined_coursecriteria']='Undefined Course Criteria';
-$string['coursecompletion_setting']='Course Completion has not enable yet';
-$string['job_title']='Job Title';
-$string['department']='Department';
-$string['joining_date']='Joining Date';
-$string['course']='Course';
-$string['report_coursecompletion']='Course Completion Report';
-$string['report_courseenrollment']='Course Enrollment Report';
-$string['module']='Module';
-$string['name']='Name';
-
+$string['pluginname'] = 'Course Status Tracker';
+$string['course_status'] = 'Course Status Tracker';
+$string['course_status_tracker'] = 'Course Status Tracker';
+$string['s_no'] = 'Serial <abbr title="number">No.</abbr>';
+$string['course_name'] = 'Course Name';
+$string['course_comp_date'] = 'Course Completion Date';
+$string['grade'] = 'Grade';
+$string['course_status_tracker:view'] = 'Course Status View';
+$string['course_status_tracker:addinstance'] = 'Add Instance';
+$string['enrolled_courses'] = 'Enrolled Courses';
+$string['completed_courses'] = 'Completed Courses';
+$string['inprogress_courses'] = 'Course In Progress';
+$string['undefined_coursecriteria'] = 'Undefined Course Criteria';
+$string['coursecompletion_setting'] = 'Course Completion has not enable yet';
+$string['job_title'] = 'Job Title';
+$string['department'] = 'Department';
+$string['joining_date'] = 'Joining Date';
+$string['course'] = 'Course';
+$string['report_coursecompletion'] = 'Course Completion Report';
+$string['report_courseenrollment'] = 'Course Enrolment Report';
+$string['module'] = 'Module';
+$string['name'] = 'Name';
